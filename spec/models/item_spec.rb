a32e95148021537ebb0173dd0c4e29e16ec205aa
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-24 03:30:41 +0900

    feat(app): Initialize the Item model

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe 'validation' do
+    context 'when name is nil' do
+      subject { build(:item, name: nil) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when price is nil' do
+      subject { build(:item, price: nil) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when price is 0' do
+      subject { build(:item, price: 0) }
+      it { is_expected.to be_valid }
+    end
+
+    context 'when price is -1' do
+      subject { build(:item, price: -1) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when price is 0.1' do
+      subject { build(:item, price: 0.1) }
+      it { is_expected.to be_invalid }
+    end
+  end
+end
