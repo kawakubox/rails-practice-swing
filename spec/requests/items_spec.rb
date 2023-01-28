3e21ab7176439eea132e8c0e0176c7a40a532f62
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-28 12:44:34 +0900

    feat(app): Implements item delete API.

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
-  describe "PUT /items/:id" do
-    let(:user) { create(:user) }
-    let(:item) { create(:item, user: user) }
+  let!(:user) { create(:user) }
+  let!(:item) { create(:item, user: user) }
 
+  describe "PUT /items/:id" do
     it 'returns updated object' do
       put item_path(item), params: { user_id: user.id, item: { name: '商品名(edited)', price: 10_000 } }
 
@@ -17,6 +17,22 @@ RSpec.describe "Items", type: :request do
     context 'when API called by other user' do
       it 'returns 403' do
         put item_path(item), params: { user_id: user.id + 1, item: { name: '商品名(edited)', price: 10_000 } }
+
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe "DELETE /items/:id" do
+    it do
+      expect {
+        delete item_path(item), params: { user_id: user.id }
+      }.to change { Item.count }.by(-1)
+    end
+
+    context 'when API called by other user' do
+      it 'returns 403' do
+        delete item_path(item), params: { user_id: user.id + 1 }
 
         expect(response).to have_http_status(:forbidden)
       end
