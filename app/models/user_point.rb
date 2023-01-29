35eb827b6599287af2592686a42651193abe658a
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-29 12:56:33 +0900

    feat(model): Implements UserPoint#spend

--- app/models/user_point.rb
+++ app/models/user_point.rb
@@ -1,3 +1,11 @@
 class UserPoint < ApplicationRecord
   belongs_to :user
+
+  validates :point, numericality: { greater_than_or_equal_to: 0 }
+
+  # @param usage [Integer]
+  def spend(usage)
+    self.point = point - usage
+    self.save!
+  end
 end
