ee0f02dc5ac45cdf2f68ab452370882e526f07be
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-30 11:27:43 +0900

    feat(app): Hashing user password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  before_validation :calculate_password_hash
+
+  private
+
+  def calculate_password_hash
+    self.password = Digest::SHA256.hexdigest(self.password) if self.password.present?
+  end
 end
