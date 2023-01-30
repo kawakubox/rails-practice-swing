c338efc9d6c324a21020af2c6e73fd7091db4678
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-30 12:22:08 +0900

    feat(model): Arrange associations on the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_one :user_point
+  has_many :items
+  has_many :transactions, through: :items
+  has_many :purchase_histories, class_name: 'Transaction', foreign_key: 'buyer_id'
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
