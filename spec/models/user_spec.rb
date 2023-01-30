557c0a96bcb36382ae3b631954cf2841a942efce
kawakubox <shigefumi.kawakubo@gmail.com>
2023-01-30 11:24:14 +0900

    feat(spec): Add validation test on the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'when nickname is nil' do
+      subject { build(:user, nickname: nil) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email is nil' do
+      subject { build(:user, email: nil) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email is already exists' do
+      let(:email) { 'sample@example.com' }
+      before { create(:user, email: email) }
+      subject { build(:user, email: email) }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when password is nil' do
+      subject { build(:user, password: nil) }
+      it { is_expected.to be_invalid }
+    end
+  end
 end
